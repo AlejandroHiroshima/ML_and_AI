d581db1fc10f190000c3096898a088cc3597f316
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-11-27 11:32:38 +0100

    Supah Cool!

---
+++ 07_b_pydantic_ai_fastapi/data_models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+class Movie(BaseModel):
+    title: str = Field(description="Title of the movie")
+    year: int = Field(gt=1970, description="year of release")
+    genre: str = Field(description="Genre of the movie, if there are many genres, take the dominant one")
+    rating: int = Field(gt=0, lt=6, description="higher rating the better, keep ratings realistic")
+
+class Prompt(BaseModel):
+    prompt: str = Field(description="""
+                        Prompt from user to find a movie, 
+                        try to find the closest movie even if the user prompts something else""")
--- 07_b_pydantic_ai_fastapi/movies.py
+++ 07_b_pydantic_ai_fastapi/movies.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from agents import movie_agent
+from data_models import Prompt
+from utils import query_duckdb
 
 app= FastAPI()
 
 @app.get("/movies")
 async def read_movies():
-    pass
+    movies = query_duckdb("FROM movies;")
+    return movies.to_dict(orient="records")
 
 @app.post("/movies/create_movie")
-async def create_movies():
-    pass
+async def create_movies(query: Prompt):
+    result = await movie_agent.run(query.prompt)
+    movie= result.output # Pydantic model
+    query_duckdb("INSERT INTO movies VALUES(?,?,?,?);", parameters=[movie.title, movie.year, movie.genre, movie.rating])
+    return movie
